b6e69360132d5386bbfbc146fc339f30eab11fa9
SiposVivien <161618821+Viviess@users.noreply.github.com>
2025-04-16 19:59:24 +0200

    SECOND

--- first.go
+++ first.go
@@ -1,6 +1,7 @@
 package main //ezen meg itt köll mindig
 
-//ez a math meg a const a konstansoknál kell
+//math a const a konstansoknál kell a számoláshoz
+//továbbiak: slices, maps,
 
 //"math"
 
@@ -165,5 +166,89 @@ func main() {
 	//	fmt.Println("2d: ", twoD)
 	//}
 
-	//SLICES (MOST POPULAR)
+	//SLICES (MOST POPULAR THEN ARRAYS)- egy egész tömb/lista stb kivágása, csak egy kis szelet-------------
+	//uninitialized=inicializálás
+	//set: elemek felsorolása
+	//get: utsó elem
+	//len: hosszúság
+	//append: hozzáad
+	//var s []string
+	//fmt.Println("uniniti: ", s, s == nil, len(s) == 0)
+
+	//s = make([]string, 3)
+	//fmt.Println("emp: ", s, "len: ", len(s), "cap: ", cap(s))
+
+	//s[0] = "a"
+	////s[1] = "b"
+	//s[2] = "c"
+	//fmt.Println("set: ", s)
+	//fmt.Println("get: ", s[2])
+	//fmt.Println("len: ", len(s))
+
+	//s = append(s, "d")
+	//s = append(s, "e", "f")
+	//fmt.Println("adp: ", s)
+	//ez itt annyi, hogy lehet őt másolni is
+	//c := make([]string, len(s))
+	//copy(c, s)
+	//fmt.Println("cpy: ", c)
+
+	//l := s[2:5]
+	//fmt.Println("sl1: ", l)
+
+	//l = s[:5]
+	//fmt.Println("sl2: ", l)
+
+	//l = s[2:]
+	//fmt.Println("sl3: ", l)
+	//így is meg lehet adni a slices elemeit
+	//t := []string{"g", "h", "i"}
+	//fmt.Println("dlc: ", t)
+
+	//t2 := []string{"g", "h", "i"}
+	//if slices.Equal(t, t2) {
+	//	fmt.Println("t == t2")
+	//}
+
+	//twoD := make([][]int, 3)
+	//for i := 0; i < 3; i++ {
+	//	innerLen := i + 1
+	//	twoD[i] = make([]int, innerLen)
+	//	for j := 0; j < innerLen; j++ {
+	//		twoD[i][j] = i + j
+	//	}
+	//}
+	//fmt.Println("2d: ", twoD)
+
+	//ASSOCIATIVE ARRAY: olyan adatszerkezet, ahol az elemeket kulcs–érték (key–value) párokban tároljuk-------------------
+
+	//m := make(map[string]int)
+
+	//m["k1"] = 7
+	////m["k2"] = 13
+	//fmt.Println("map", m)
+
+	//v1 := m["k1"]
+	////fmt.Println("v1: ", v1)
+	//v3 := m["k3"]
+	//fmt.Println("v3: ", v3)
+
+	//fmt.Println("len: ", len(m))
+	//delete(m, "k2")
+	//fmt.Println("map: ", m)
+
+	//clear(m)
+	//fmt.Println("map: ", m)
+	//prs. present
+	//_, prs := m["k2"]
+	//fmt.Println("prs: ", prs)
+
+	//n := map[string]int{"foo": 1, "bar": 2}
+	//fmt.Println("map: ", n)
+
+	//n2 := map[string]int{"foo": 1, "bar": 2}
+	//if maps.Equal(n, n2) {
+	//	fmt.Println("n == n2")
+	//}
+
 }
