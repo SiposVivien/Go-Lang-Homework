836b787678963773e963d20c470430c52581206f
SiposVivien <161618821+Viviess@users.noreply.github.com>
2025-04-21 17:35:48 +0200

    FOURTH

--- try.go
+++ try.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
-	//"net/http"
-	//"time"
-	"os"
-	//"os/signal"
-	//"syscall"
-	"bufio"
+	"net/http"
 )
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
 
 //func first(w http.ResponseWriter, req *http.Request) {
 //	ctx := req.Context()
@@ -28,48 +37,53 @@ import (
 //}
 // z := store.NewMemoryStore()
 
-func NewMemoryStore() *MemoryStore {
-	m := MemoryStore{
-		items: []int{},
-		zz:    make(chan int),
-	}
-	go m.start()
-	return &m
-}
+//func NewMemoryStore() *MemoryStore {
+//	m := MemoryStore{
+//		items: []int{},
+//		zz:    make(chan int),
+//	}
+//	go m.start()
+//	return &m
+//}//
 
 func main() {
 
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("timestamp: ")
-	scanner.Scan()
-	utime := scanner.Text()
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
 
-	func(m *NewMemoryStore) {
-		for {
-			select {
-			case utime := <-m.zz:
-				m.item = append(m.item, utime)
-			}
-		}
-	}
-
-	//	http.HandleFunc("/hello", first)
-	//	http.ListenAndServe(":8090", nil)
-
-	//sigs := make(chan os.Signal, 1)
-
-	//signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-	//done := make(chan bool, 1)
-
-	//go func() {
-	//	sig := <-sigs
-	//	fmt.Println()
-	//	fmt.Println(sig)
-	//	done <- true
-	//}()
-
-	//fmt.Println("await signal")
-	//<-done
-	//fmt.Println("exiting")
+	http.ListenAndServe(":8090", nil)
 }
+
+//	scanner := bufio.NewScanner(os.Stdin)
+//	fmt.Println("timestamp: ")
+//	scanner.Scan()
+//	utime := scanner.Text()
+
+//	func(m *NewMemoryStore) {
+//	for {
+//			select {
+//			case utime := <-m.zz:
+//			m.item = append(m.item, utime)
+//			}
+//	}
+//}
+
+//	http.HandleFunc("/hello", first)
+//	http.ListenAndServe(":8090", nil)
+
+//sigs := make(chan os.Signal, 1)
+
+//signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+//done := make(chan bool, 1)
+
+//go func() {
+//	sig := <-sigs
+//	fmt.Println()
+//	fmt.Println(sig)
+//	done <- true
+//}()
+
+//fmt.Println("await signal")
+//<-done
+//fmt.Println("exiting")
